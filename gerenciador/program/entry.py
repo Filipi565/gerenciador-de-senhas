c0f88d872aef8e830e2a4da374ffd484be20e0ba
Filipi565 <116827820+Filipi565@users.noreply.github.com>
2023-11-17 10:45:20 -0300

    Create entry.py

---
+++ gerenciador/program/entry.py
@@ -0,0 +1,27 @@
+from tkinter import *
+
+class Entry(Entry):
+    def __init__(self, master=None, placeholder='', color='grey', **kwargs):
+        super().__init__(master, **kwargs)
+
+        self._placeholder = placeholder
+        self._placeholder_color = color
+        self._default_fg_color = self['fg']
+
+        self.bind("<FocusIn>", self.__foc_in)
+        self.bind("<FocusOut>", self.__foc_out)
+
+        self.__put_placeholder()
+
+    def __put_placeholder(self):
+        self.insert(0, self._placeholder)
+        self['fg'] = self._placeholder_color
+
+    def __foc_in(self, *args):
+        if self['fg'] == self._placeholder_color:
+            self.delete('0', 'end')
+            self['fg'] = self._default_fg_color
+
+    def __foc_out(self, *args):
+        if not self.get():
+            self.__put_placeholder()
